301c49c209a2d1980c8fdd47377e9ebef8618ac1
Dodo895 <carbuneincins@gmail.com>
2026-07-16 12:01:47 +0300

    Robot teams da mesaje

--- Selenium.py
+++ Selenium.py
@@ -4,6 +4,7 @@ import time
 import sqlite3
 import datetime
 
+
 def curata_pretul(pret_text):
     if pret_text == "Pret indisponibil/Epuizat":
         return 0.0
@@ -13,58 +14,59 @@ def curata_pretul(pret_text):
     return float(pret)
 
 
-conn = sqlite3.connect("emag_tracker.db")
-cursor = conn.cursor()
+    conn = sqlite3.connect("emag_tracker.db")
+    cursor = conn.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS istoric_preturi (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nume_produs TEXT,
-    pret REAL,
-    data_verificare TEXT
-)
-""")
-conn.commit()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS istoric_preturi (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nume_produs TEXT,
+        pret REAL,
+        data_verificare TEXT
+    )
+    """)
+    conn.commit()
 
-print("Pornim robotul...")
-browser = webdriver.Chrome()
-lista_produse = [
-    "https://www.emag.ro/biblia-sau-sfanta-scriptura-format-053-alba-patriarhia-romana-cns322/pd/D1GGX6BBM/",
-    "https://www.emag.ro/banda-de-alergat-electrica-orion-sprint-c1-bluetooth-fitshow-kinomap-zwift-viteza-maxima-12-km-h-greutate-maxima-suportata-100kg-pliabila-12-programe-presetate-sprintc1/pd/D6SWG8BBM/",
-    "https://www.emag.ro/telefon-mobil-samsung-galaxy-a16-dual-sim-128gb-4gb-ram-4g-light-green-sm-a165flgbeue/pd/DJJ1YZYBM/"
-]
+    print("Pornim robotul...")
+    browser = webdriver.Chrome()
+    lista_produse = [
+        "https://www.emag.ro/biblia-sau-sfanta-scriptura-format-053-alba-patriarhia-romana-cns322/pd/D1GGX6BBM/",
+        "https://www.emag.ro/banda-de-alergat-electrica-orion-sprint-c1-bluetooth-fitshow-kinomap-zwift-viteza-maxima-12-km-h-greutate-maxima-suportata-100kg-pliabila-12-programe-presetate-sprintc1/pd/D6SWG8BBM/",
+        "https://www.emag.ro/telefon-mobil-samsung-galaxy-a16-dual-sim-128gb-4gb-ram-4g-light-green-sm-a165flgbeue/pd/DJJ1YZYBM/"
+    ]
 
 
-while True:
-    print("Se da refresh la preturi")
+    while True:
+        print("Se da refresh la preturi")
     
    
-    for i in lista_produse:
-        browser.get(i)
-        time.sleep(3)
+        for i in lista_produse:
+            browser.get(i)
+            time.sleep(3)
 
-        titlu = browser.find_element(By.TAG_NAME,"h1").get_attribute("textContent").strip()
+            titlu = browser.find_element(By.TAG_NAME,"h1").get_attribute("textContent").strip()
 
-        try:
-            pret_brut = browser.find_element(By.CSS_SELECTOR,".product-new-price").get_attribute("textContent").strip()
-        except:
-            pret_brut = "Pret indisponibil/Epuizat"
+            try:
+                pret_brut = browser.find_element(By.CSS_SELECTOR,".product-new-price").get_attribute("textContent").strip()
+            except:
+                pret_brut = "Pret indisponibil/Epuizat"
 
-        pret_curat = curata_pretul(pret_brut)
-        data_acum = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            pret_curat = curata_pretul(pret_brut)
+            data_acum = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         
-        cursor.execute("""
-        INSERT INTO istoric_preturi (nume_produs, pret, data_verificare)
-        VALUES (?, ?, ?)
-        """, (titlu, pret_curat, data_acum))
-        conn.commit()
+            cursor.execute("""
+            INSERT INTO istoric_preturi (nume_produs, pret, data_verificare)
+            VALUES (?, ?, ?)
+            """, (titlu, pret_curat, data_acum))
+            conn.commit()
         
-        print(f"Salvat in DB: {titlu} | {pret_curat} lei")
+            print(f"Salvat in DB: {titlu} | {pret_curat} lei")
     
    
-    print("Asteptam 60 de secunde...")
-    time.sleep(60)
+        print("Asteptam 60 de secunde...")
+        time.sleep(60)
 
-browser.quit()
-conn.close()
-print("S-a extras cu succes")
+    browser.quit()
+    conn.close()
+    print("S-a extras cu succes")
+
---
+++ teams.py
@@ -0,0 +1,62 @@
+﻿from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+def trimite_mesaje_teams():
+    lista_utilizatori=[
+        "andrei-valentin.lazarescu@stud.mta.ro",
+        "daniel-iosif.ogrin@stud.mta.ro",
+        "mircea-florin.corbu@stud.mta.ro"
+        ]
+    mesaj_de_trimis="Daca vezi asta deja e un indian in pc-ul tau fugi."
+    print("Pornim teams")
+    optiuni = Options()
+    optiuni.add_argument('--no-sandbox')               
+    optiuni.add_argument('--disable-dev-shm-usage')   
+    
+    browser = webdriver.Chrome(options=optiuni)
+    browser.get("https://teams.microsoft.com/")
+
+    print("Ai 150 secunde sa completezi")
+    time.sleep(150)
+
+    wait=WebDriverWait(browser,15)
+
+    for utilizator in lista_utilizatori:
+
+        try:
+            bara_cautare=wait.until(EC.element_to_be_clickable((By.XPATH,'//input[contains(@placeholder, "Search") or contains(@placeholder, "Căutați") or @role="combobox"]')))
+            bara_cautare.send_keys(Keys.CONTROL + "a")
+            time.sleep(0.5)
+            bara_cautare.send_keys(Keys.DELETE)
+            time.sleep(1)
+            bara_cautare.send_keys(utilizator)
+            print("Se cauta utilizatorul")
+            time.sleep(4)
+            bara_cautare.send_keys(Keys.ARROW_DOWN)
+            time.sleep(1)
+            bara_cautare.send_keys(Keys.ENTER)
+            print("Am selectat chatul")
+            time.sleep(6)
+            casuta_mesaj = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@contenteditable="true"]')))
+            casuta_mesaj.click()
+            time.sleep(1)
+            casuta_mesaj.send_keys(mesaj_de_trimis)
+            time.sleep(1)
+            casuta_mesaj.send_keys(Keys.ENTER)
+            print(f"=> Mesajul catre {utilizator} a fost trimis")
+        except:
+            print(f"Eroare")
+        time.sleep(4)
+
+
+    print("Toate mesajele au fost transmise")
+    browser.quit()
+
+trimite_mesaje_teams()
+
+
+
